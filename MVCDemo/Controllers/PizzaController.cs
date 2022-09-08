d92b7a8d4323eb1ec1b86e543c63d113699919ab
ETG1 <ETG1@H9BKX52-SHEL>
2022-09-08 18:11:37 +0530

    added

---
+++ MVCDemo/Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+using MVCDemo.Service;
+
+namespace MVCDemo.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzas = PizzaService.GetAll();
+            return View(pizzas);
+        }
+        public IActionResult List()
+        {
+            List<Pizza> pizzas = PizzaService.GetAll();
+            return View(pizzas);
+        }
+        public IActionResult Details(int id)
+        {
+            Pizza p= PizzaService.Get(id);
+            return View(p);
+        }
+
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        //public IActionResult Create(int id,string name,int size,decimal price,bool Isglutenfreee)
+        public IActionResult Create(Pizza p)
+        {
+            PizzaService.Add(p);
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ WebAppDETAug2022/Services/IHelo.cs
@@ -0,0 +1,21 @@
+﻿namespace WebAppDETAug2022.Services
+{
+    public interface IHelo
+    {
+        string SayHelo(string name);
+    }
+    public class Helo1 : IHelo
+    {
+        public string SayHelo(string name)
+        {
+            return $"Helo {name}, Welcome tp ASP.Net core";
+        }
+    }
+    public class Helo2 : IHelo
+    {
+        public string SayHelo(string name)
+        {
+            return $"Hai, Helo {name}, Howz the day!";
+        }
+        }
+}
