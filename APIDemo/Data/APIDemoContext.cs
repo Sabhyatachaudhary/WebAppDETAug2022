8c2bfbf47f876f8b0015c6c7f8b0f9bef70382fd
ETG1 <ETG1@H9BKX52-SHEL>
2022-09-13 18:55:19 +0530

    add

--- APIDemo/Data/APIDemoContext.cs
+++ APIDemo/Data/APIDemoContext.cs
@@ -15,5 +15,7 @@ namespace APIDemo.Data
         }
 
         public DbSet<APIDemo.Models.User> User { get; set; } = default!;
+
+        public DbSet<APIDemo.Models.TodoItem>? TodoItem { get; set; }
     }
 }
